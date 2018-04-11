21e400e3c16083e023bdd40f9089140b94d7d7e0
Santosh Kumar <sntshkmr60@gmail.com>
2018-04-12 02:31:10 +0530

    plays background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer instance = null;
+    // Use this for initialization
+    void Start () {
+        if (instance != null) {
+            Destroy(gameObject);
+            print("Duplicate music player self-desructing");
+        } else {
+            instance = this;
+        }
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
+
+// TODO: 
+// 1. Randomize playback
+// 2. Pause and reactivate music when user pauses the game
+// 3. Animate the music start.
