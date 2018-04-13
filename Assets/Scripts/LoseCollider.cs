43d39024d3018ca867027e90ec5792ae60228a14
Santosh Kumar <sntshkmr60@gmail.com>
2018-04-14 04:20:58 +0530

    Collision loads new level

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
     void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log("Collision");
     }
 
     void OnTriggerEnter2D(Collider2D trigger) {
         Debug.Log("Trigger");
+        levelManager.LoadLevel("Win");
     }
 }
